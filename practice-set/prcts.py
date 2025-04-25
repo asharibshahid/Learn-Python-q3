add51f6baedf8cc22cc24dc5bdbc135b36a2cd33
ASHARIB-SHEIKH👩🏻‍💻 <160227183+asharibshahid@users.noreply.github.com>
2025-04-26 01:58:35 +0500

    Update prcts.py

--- practice-set/prcts.py
+++ practice-set/prcts.py
@@ -218,6 +218,8 @@
 
 
 #  Functions 
+
+
 # def  printingMyName(name , age):
 #     print(f"my Name Is {name} and My Age is {age}")
 #     if age >= 18:
@@ -225,7 +227,7 @@
 #     elif age >= 17:
 #             print("Please Wait for One Year")
 #     else:
-#         print("Sorry")
+#         print("Sorry You Are Under Age ")
 
 
 
@@ -234,18 +236,251 @@
 
 # printingMyName(nAmee, int(AAgee))
 
+
+
 #  Oops
 
 
-class Employee:
+# class Employee:
    
-    def __init__(self,name,salary):
-        self.name = name
-        self.salary = salary
-    def getSalary(self):
-          return self.salary
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+#     def getSalary(self):
+#           return self.salary
 
 
-asharib = Employee("Asharib", "100000")
-print(asharib.salary)
-print(asharib.name)
+# asharib = Employee("Asharib", "100000")
+# print(asharib.salary)
+# print(asharib.name)
+
+
+# Sets
+# Ye Double Value ko ignre krta hai aur Ye Aik Unordered Collection hai
+#  Aur iska Data Type Set hai
+
+# set1 = {1,4,6,"asharib" , "hello", "hello"}
+# print (set1)
+# print(type(set1))
+
+# my_dict = {
+#     "cat": "a Small animal",
+#     "table": ["A furniture", "A place to study"],
+# }
+
+# cls_count =  { "python" ,"python" ,"javascript" , "typescript" , "python" , "java" , "typescript" }
+# final = len(cls_count)
+
+# print(f"We Wanna {final} Classrooms") 
+
+# colors ={"red" , "blue" , "green" , "yellow"}
+# for i in colors:
+#     print(i)
+#     if i == "red":
+#        print(i+"Humm")
+#     elif i == "yellow":
+#         print(i+ "  Ye hy yellow")
+
+
+# for i in range(2,90):
+#     print(f"A{i}")
+
+
+# class person():
+#     name ="asharib",
+#     age = 16,
+#     love = "None",
+
+
+
+# a = person()     # Now Just asharib 
+# a.name = "Sheikh"  #Now Sheikh Instead Of asharib
+
+
+# print(a.name)
+
+
+# class person:
+#     name ="asharib",
+#     age = 16,
+#     love = "None",
+#     occ = " Developer"
+#     def Info(self):
+#         print(f"{self.name} is a {self.occ}")
+
+# b = person()
+# b.I()
+
+# def q1(name):
+#     print(f"Hello {name} WAelcome")
+
+# q1(input("Enter Your Name "))    
+# student = {"name": "Ahmed", "age": 18}
+
+# for value in student.items():
+#     print(f" {value}")
+
+# List:int = [5, 10, 15] 
+# def avrg(List):
+#      avg= sum(List) / len(List)
+#      print(f"Average is {avg}")
+
+# avrg(List)     
+
+# nums = [1, 2, 3, 4, 5]
+# print(nums[::-1])  # 
+
+
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# def Check_Vowel(vowels):
+#     name = input("Enter Your Name ")
+#     if name.lower() in vowels:
+#         print(f"{name} is a Vowel")
+#     else:
+#         print(f"{name} is not a Vowel")
+
+
+
+# Check_Vowel(vowels)
+
+# my_dict={"name":"asharib" , "age": 23, "address": {"location": "karachi"}  }
+# my_set = {1, 2, 3, 4, 5}
+# for my_set in range(5):
+#     print(my_set)
+
+# a={1,2,3,4,5}
+# b={4,5,6,7,8}
+# Nums = a.union(b)
+# print(Nums)
+
+
+# my_set = {1, 2, 3, 4, 5}
+# my_set2 = {4, 5, 1, 7, 2}
+# intersection= my_set.intersection(my_set2)
+# print(intersection)
+
+
+# file_name= input("Enter Your File Name ")
+# with open(file_name, "r") as file:
+#     for line in file:
+#         words=line.split()
+#         print(words)
+# print("Please Enter Words To Count The Number of Words")
+# words = input("Enter a string: ")
+# def word_counter(words):
+#     word_nospace = words.replace(" ", "")
+  
+#     word_count = len(word_nospace)
+#     print(f"The number of words in the string is: {word_count}")
+
+
+# word_counter(words)    
+
+
+# Data = [0,4,3,2,3,5,8,9,10]
+# Data.sort()
+# def find_Even_odd(Data):
+#     even = []
+#     odd = []
+   
+#     for i in Data:
+#         if i % 2 == 0:
+#            even.append(i)
+#         else:
+#             odd.append(i)
+#     print(f"Even Numbers are {even}")
+#     print(f"Odd Numbers are {odd}")
+
+
+
+
+# find_Even_odd(Data)    
+
+
+# put_name = input("Enter the 1st String ")
+# put_name.split(",")
+# put_name2 = input("Enter the 1st String ")
+# put_name2.split(",")
+# set(put_name)
+# set(put_name2)
+# common = set(put_name).intersection(set(put_name2))
+# print(f"Common Elements are {common}")
+
+# inp=input("Enter Your String ")
+
+
+# def justAlphabets(Inp):
+#     if inp.isalpha():
+#         print("String is Alphabetic")
+#     else:
+#         print("String is not Alphabetic")
+# justAlphabets(inp)        
+
+
+# my_dict = {"apple": 3, "banana": 1, "cherry": 2}
+# sorted_dict = dict(sorted(my_dict.items(), key=lambda item: item[1]))
+# print(sorted_dict)
+
+# get_num = input("Enter Your Number to perform operation:  ")
+
+# get_num2 = input("Enter Your 2nd Number to perform operation:  ")
+# ops:int = input('Please enter The Operation you want to - , + , x , % :  ')
+# for op in ops:
+#     if op == '-':
+#         print(int(get_num) - int(get_num2))
+#     elif op == '+':    
+#         print(int(get_num) + int(get_num2))
+#     elif op == 'x':
+#         print(int(get_num) * int(get_num2))
+#     elif op == '%':
+#         print(int(get_num) / int(get_num2))
+#     else:
+#         print("Please Enter Valid Operation")
+#         break
+
+
+
+# students = {
+#     "Ali": [85, 90, 78],
+#     "Sara": [88, 76, 92],
+#     "Zain": [95, 91, 89]
+# }
+# ali = students["Ali"]
+# avrg1 = sum(ali) / len(ali)
+# zain = students["Zain"]
+# avrg2 = sum(zain) / len(zain)
+# sara = students["Sara"]
+# avrg3 = sum(sara) / len(sara)
+
+# final_avaerage= print(avrg1,avrg2,avrg3)
+
+
+
+
+# class Private:
+#     def _init_(self , name , salary):
+#         self.name = name 
+#         self.salary = salary
+        
+  
+#         print(f"The name is {name}  and your salary is {salary}")   
+
+# Private("ayesha" , 890)
+
+
+# class Students:
+#     def __init__(self , name , marks):
+#         self.name = name
+#         self.marks = marks
+
+#     def average(self):
+#         plus = sum(self.marks) / len(self.marks)
+#         print(f"Average Marks of {self.name} is {plus}")
+
+
+
+# s1 = Students("Ali", [85, 90, 78])
+# print(s1.marks)
+# s1.average()
+
+
